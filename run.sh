4052fa9143b3e0df85241ff9bdc3b86e25776c13
sycured <60801403+sycured@users.noreply.github.com>
2020-09-05 01:04:21 -0500

    fix locale

--- run.sh
+++ run.sh
@@ -6,6 +6,8 @@ buildah config --author='sycured' $mkimg
 buildah config --label Name='centos-haskell-builder' $mkimg
 buildah run "$mkimg" -- useradd -m hsk
 buildah run "$mkimg" -- dnf upgrade -y
+buildah run "$mkimg" -- dnf install glibc-langpack-en -y
+buildah config --env LANG="en_US.UTF8" --env LC_ALL="en_US.UTF8" $mkimg
 buildah run "$mkimg" -- bash -c "curl -sSL https://get.haskellstack.org/ | sh"
 buildah run "$mkimg" -- rm -rf /var/cache/dnf/*
 buildah config --user='hsk' $mkimg
